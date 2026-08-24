bca05718ee1731949a1962ddff2fd6cdfdf2044b
Benjamin DalFavero <bdalfavero@gmail.com>
2026-08-24 13:37:14 -0700

    Ran XXZ scaling at a small size.

--- examples/Scaling/plots.py
+++ examples/Scaling/plots.py
@@ -7,10 +7,11 @@ app = marimo.App(width="medium")
 @app.cell
 def _():
     import pandas as pd
+    import numpy as np
     import matplotlib.pyplot as plt
     import seaborn as sns
 
-    return pd, plt
+    return np, pd, plt
 
 
 @app.cell
@@ -31,6 +32,61 @@ def _(df, plt):
     ax.set_ylabel("Aboslute error", color="blue")
     ax.set_xlabel("Iteration")
     right_ax.set_ylabel("Iteration time", color="red")
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell
+def _():
+    import marimo as mo
+
+    return (mo,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## XXZ
+    """)
+    return
+
+
+@app.cell
+def _(pd):
+    df_xxz = pd.read_csv("xxz_results.csv")
+    print(df_xxz.head())
+    return (df_xxz,)
+
+
+@app.cell
+def _(df_xxz, np):
+    l_vals = np.unique(df_xxz["N"])
+    chi_vals = np.unique(df_xxz["chi"])
+    print(l_vals, chi_vals)
+    return chi_vals, l_vals
+
+
+@app.cell
+def _(chi_vals, df_xxz, l_vals, np, plt):
+    fig_xxz, ax_xxz = plt.subplots(len(l_vals), len(chi_vals))
+
+    for i, l in enumerate(np.unique(df_xxz["N"])):
+        for j, chi in enumerate(np.unique(df_xxz["chi"])):
+            ax_xxz[i, j].set_title(f"L={l} chi={chi}")
+            df_queried = df_xxz.query(f"N=={l} and chi=={chi}")
+            right_ax_xxz = ax_xxz[i,j].twinx()
+            ax_xxz[i,j].plot(df_queried["iteration"], df_queried["abs_error"], color="blue")
+            right_ax_xxz.plot(df_queried["iteration"], df_queried["time"], color="red")
+            ax_xxz[i,j].set_ylabel("Aboslute error", color="blue")
+            ax_xxz[i,j].set_xlabel("Iteration")
+            right_ax_xxz.set_ylabel("Iteration time", color="red")
+
+    fig_xxz.tight_layout()
+    plt.show()
     return
 
 
---
+++ examples/Scaling/xxz_tn_scaling.py
@@ -0,0 +1,127 @@
+from time import perf_counter_ns
+import numpy as np
+import pandas as pd
+from openfermion import MolecularData
+from openfermionpyscf import run_pyscf
+from adaptvqe.algorithms.adapt_vqe import TensorNetAdapt, LinAlgAdapt
+from adaptvqe.pools import DVE_CEO, FullPauliPool, TiledPauliPool
+from adaptvqe.hamiltonians import XXZHamiltonian
+
+MAX_MPO_BOND = 200
+NUM_ITER = 10
+
+max_mpo_bond = 100
+dmrg_mps_bond = 10
+adapt_mps_bond = 5
+l = 4
+
+j_xy = 1
+j_z = 1
+h = XXZHamiltonian(j_xy, j_z, l, diag_mode="quimb", max_mpo_bond=max_mpo_bond, max_mps_bond=dmrg_mps_bond)
+dmrg_energy = h.ground_energy
+print(f"Got DMRG energy {dmrg_energy:4.5e}")
+pool = FullPauliPool(n=l, max_mpo_bond=max_mpo_bond)
+
+# Run 200 iterations of ADAPT-VQE for small problem instance, selecting randomly among degenerate gradients.
+# Form a list of all unique operators ever selected for this small instance.
+ixs = []
+for _ in range(100):
+    my_adapt = TensorNetAdapt(
+        pool=pool,
+        custom_hamiltonian=h,
+        verbose=False,
+        threshold=10**-5,
+        max_adapt_iter=5,
+        max_opt_iter=10000,
+        sel_criterion="gradient",
+        recycle_hessian=False,
+        rand_degenerate=True,
+        max_mpo_bond=100,
+        max_mps_bond = 20
+    )
+    my_adapt.run()
+    data = my_adapt.data
+    for i in data.result.ansatz.indices:
+        if i not in ixs:
+            ixs.append(i)
+
+print(f"Pool will be tiled from {len(ixs)} ops")
+source_ops = [pool.operators[index].operator for index in ixs]
+
+class ScalingResult:
+    def __init__(self, N: int, chi: int, exact_energy: float, times: list[float], energies: list[float]):
+        assert len(times) == len(energies)
+
+        self._N = N
+        self._chi = chi
+        self._exact_energy = exact_energy
+        self._times = np.array(times)
+        self._energies = np.array(energies)
+
+    def to_dataframe(self) -> pd.DataFrame:
+        abs_errors = np.abs(self._energies - self._exact_energy)
+        rel_errors = abs_errors / abs(self._exact_energy)
+
+        df = pd.DataFrame(
+            data={
+                "iteration": np.array(range(self._times.size)),
+                "time": self._times, "energy": self._energies,
+                "abs_error": abs_errors, "rel_error": rel_errors
+            }
+        )
+        df["N"] = self._N
+        df["chi"] = self._chi
+        # df.set_index("iteration", inplace=True)
+        return df
+
+
+def run_n_chi(N: int, chi: int, num_iter: int=NUM_ITER):
+    new_l = N
+    j_xy = 1
+    j_z = 1
+    h = XXZHamiltonian(
+        j_xy, j_z, new_l,
+        store_ref_vector=False,
+        diag_mode="quimb", max_mpo_bond=MAX_MPO_BOND, max_mps_bond=dmrg_mps_bond
+    )
+    dmrg_energy = h.ground_energy
+    print(f"Got DMRG energy {dmrg_energy:4.5e}")
+    tiled_pool = TiledPauliPool(n=new_l, source_ops=source_ops)
+
+    tn_adapt = TensorNetAdapt(
+        pool=tiled_pool,
+        custom_hamiltonian=h,
+        verbose=True,
+        threshold=10**-5,
+        max_adapt_iter=30,
+        max_opt_iter=10000,
+        sel_criterion="gradient",
+        recycle_hessian=False,
+        rand_degenerate=True,
+        max_mpo_bond=max_mpo_bond,
+        max_mps_bond=adapt_mps_bond
+    )
+    tn_adapt.initialize()
+
+    adapt_energies = []
+    adapt_times = []
+    for _ in range(num_iter):
+        start_time = perf_counter_ns()
+        tn_adapt.run_iteration()
+        end_time = perf_counter_ns()
+        elapsed_time = end_time - start_time
+        adapt_energies.append(tn_adapt.energy)
+        adapt_times.append(elapsed_time)
+    return ScalingResult(N, chi, dmrg_energy, adapt_times, adapt_energies)
+
+
+if __name__ == "__main__":
+    dfs = []
+    for N in [8, 10]:
+        for chi in [5, 7]:
+            print(f"N={N} chi={chi}")
+            result = run_n_chi(N, chi, num_iter=5)
+            df = result.to_dataframe()
+            dfs.append(df)
+            total_df = pd.concat(dfs)
+            total_df.to_csv("xxz_results.csv", index=False)
